f362d55fac8c85db807215a6e5b6b07f2b7239c2
_jiankang <1374964816@qq.com>
2022-07-28 13:44:33 +0800

    gitlab-runner

--- src/main/java/com/jiankang/diary/service/impl/SysUserDetailServiceImpl.java
+++ src/main/java/com/jiankang/diary/service/impl/SysUserDetailServiceImpl.java
@@ -1,8 +1,21 @@
 package com.jiankang.diary.service.impl;
 
+import com.jiankang.diary.mapper.SysUserDetailMapper;
+import com.jiankang.diary.model.SysUserDetail;
 import com.jiankang.diary.service.SysUserDetailService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @Service("sysUserDetailService")
 public class SysUserDetailServiceImpl implements SysUserDetailService {
+
+    @Resource
+    private SysUserDetailMapper sysUserDetailMapper;
+
+    @Override
+    public void save(SysUserDetail sysUserDetail) {
+        sysUserDetailMapper.insert(sysUserDetail);
+    }
 }
